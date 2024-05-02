b7f8c21c6f53966a3875dfc7263cbfde76973089
Ilya Bogatov <111486509+Cuga77@users.noreply.github.com>
2024-05-02 11:15:04 +0300

    Update VertexColoring3Periodic.java

--- java/VertexColoring3Periodic.java
+++ java/VertexColoring3Periodic.java
@@ -6,8 +6,8 @@ import com.mathsystem.domain.plugin.plugintype.GraphProperty;
 
 import java.util.*;
 
-public class VertexColoring3Periodic  {
-    // Метод для проверки 3-периодичности раскраски графа
+public class VertexColoring3Periodic implements GraphCharacteristic {
+    @Override
     public boolean is3Periodic(Graph graph) {
         // Проходим по всем вершинам графа
         for (Vertex vertex : graph.getVertices()) {
@@ -19,7 +19,7 @@ public class VertexColoring3Periodic  {
                 Vertex neighbor = edge.getOtherVertex(vertex);
                 // Добавляем цвет соседней вершины в множество
                 colors.add(neighbor.getColor());
-                // Если в множестве более трех цветов, то раскраска не является 3-периодической
+                // Если в множестве более двух цветов, то раскраска не является 3-периодической
                 if (colors.size() > 3) {
                     return false;
                 }
